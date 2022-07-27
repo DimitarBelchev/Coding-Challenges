3ad348dc38693a0a5dfaef4a7e05f30dbf8072df
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-07-27 16:10:44 +0300

    Update Twisted Sum.js

--- codewars/Twisted Sum.js
+++ codewars/Twisted Sum.js
@@ -10,31 +10,30 @@
 // # N = 12
 // 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + (1 + 0) + (1 + 1) + (1 + 2) = 51
 
-// function twistedSum(n) {
-//   let arr = [];
-//   sum = 0;
-//   let i = 1;
-//   while (i <= n) {
-//     sum += i % 10;
-//     arr.push(sum);
-//     console.log(arr);
-//     // i = Math.floor(i / 10);
-
-//     // console.log(i);
-//   }
-//   return (sumArr = arr.reduce((a, b) => a + b, 0));
-// }
-
-// console.log(twistedSum(100));
-
 function twistedSum(n) {
-  let finalArr = [];
-  for (i = 1; i <= n; i++) {
-    finalArr.push(i.toString().split(""));
+  let arr = [];
+  sum = 0;
+  while (i <= n) {
+    sum += i % 10;
+    arr.push(sum);
+    console.log(arr);
+    // i = Math.floor(i / 10);
+
+    // console.log(i);
   }
-  return (sum = finalArr
-    .flat()
-    .map((str) => Number(str))
-    .reduce((a, b) => a + b, 0));
+  return (sumArr = arr.reduce((a, b) => a + b, 0));
 }
+
 console.log(twistedSum(12));
+
+// function twistedSum(n) {
+//   let finalArr = [];
+//   for (i = 1; i <= n; i++) {
+//     finalArr.push(i.toString().split(""));
+//   }
+//   return (sum = finalArr
+//     .flat()
+//     .map((str) => Number(str))
+//     .reduce((a, b) => a + b, 0));
+// }
+// console.log(twistedSum(12));
