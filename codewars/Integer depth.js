c374d7ea870aeb1fe2053de085435b968aec2cbe
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-08-30 16:51:48 +0300

    55

---
+++ codewars/Integer depth.js
@@ -0,0 +1,20 @@
+function computeDepth(x) {
+  let counter = 1;
+  let numArr = [];
+  const reqNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  while (!reqNumbers.every((i) => numArr.includes(i))) {
+    numArr.push(Array.from(String(x * counter), Number));
+    numArr = numArr.reduce((acc, val) => acc.concat(val), []);
+    counter++;
+  }
+  return counter - 1;
+}
+console.log(computeDepth(42));
+
+// const array2 = numArr;
+
+//   const containAll = array1.every((item) => {
+//     return array2.includes(item);
+//   });
+
+//   console.log(containAll);
---
+++ codewars/Nested List Depth.js
@@ -0,0 +1,5 @@
+function getArrayDepth(value) {
+  return Array.isArray(value)
+    ? 1 + Math.max(0, ...value.map(getArrayDepth))
+    : 0;
+}
