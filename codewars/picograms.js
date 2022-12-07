bd7a327031759ed7bbd821463633eaf232a4fcdf
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-12-07 23:55:45 +0200

    Delete picograms.js

--- codewars/picograms.js
+++
@@ -1,9 +0,0 @@
-function isIsogram(str) {
-  str = str.toLowerCase().split("");
-  const lettersUnique = [...new Set(str)];
-  const lettersIf = lettersUnique.length === str.length ? true : false;
-  return lettersIf;
-  //...
-}
-console.log(isIsogram("aba"));
-console.log(isIsogram("Dermatoglyphics"));
