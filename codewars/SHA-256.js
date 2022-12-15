670ceb3b141e8613b526f328d1de393de80b9389
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-12-15 03:31:52 +0200

    uo

--- codewars/SHA-256.js
+++ codewars/SHA-256.js
@@ -2,9 +2,25 @@
 
 // "Hello World!" => "7f83b1657ff1fc53b92dc18148a1d65dfc2d4b1fa3d677284addd200126d9069"
 
-function sha256(string) {
-  const crypto = require("crypto");
-  hashes = crypto.getHashes();
-  return crypto.createHash("sha256").update(string).digest("hex");
+// function sha256(string) {
+//   const crypto = require("crypto");
+//   hashes = crypto.getHashes();
+//   return crypto.createHash("sha256").update(string).digest("hex");
+// }
+// console.log(sha256("Hello World!"));
+
+function asciiToHexSha256(asciiString) {
+  // Create a new instance of the jsSHA library
+  const jsSHA = require("jssha");
+  const sha256 = new jsSHA("SHA-256", "ASCII");
+
+  // Update the hash with the given ascii string
+  sha256.update(asciiString);
+
+  // Get the hexadecimal hash of the string
+  const hexHash = sha256.getHash("HEX");
+
+  return hexHash;
 }
-console.log(sha256("Hello World!"));
+
+console.log(asciiToHexSha256("Hello World!"));
