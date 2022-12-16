42ce971694843752988e650e5475cfa003d3a773
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2022-12-16 22:01:42 +0200

    Update +1 Array.js

--- codewars/+1 Array.js
+++ codewars/+1 Array.js
@@ -1,20 +1,15 @@
 function upArray(arr) {
-  if (
-    !Array.isArray(arr) ||
-    !arr.every((x) => typeof x === "number" && x >= 0 && x < 10) ||
-    arr.length === 0
-  ) {
-    return null;
+  if (!arr.every((ele) => ele >= 0 && ele <= 9) || arr.length == 0) return null;
+  arr[arr.length - 1]++;
+  for (let i = arr.length - 1; i > 0; i--) {
+    if (arr[i] > 9) {
+      arr[i] = 0;
+      arr[i - 1]++;
+    }
   }
-  const result = new Array(arr.length).fill(0);
-  let carry = 1;
-  for (let i = arr.length - 1; i >= 0; i--) {
-    result[i] = (arr[i] + carry) % 10;
-    carry = Math.floor((arr[i] + carry) / 10);
+  if (arr[0] > 9) {
+    arr[0] = 0;
+    arr.unshift(1);
   }
-  if (carry > 0) {
-    result.unshift(carry);
-  }
-
-  return result;
+  return arr;
 }
