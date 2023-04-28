e7dc209e146572ef8071ded9c899eadb91820c90
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-04-28 23:04:17 +0300

    Update Remove the time.js

--- codewars/Remove the time.js
+++ codewars/Remove the time.js
@@ -2,3 +2,4 @@ function shortenToDate(longDate) {
   const dateParts = longDate.split(", ");
   return dateParts[0] + " " + dateParts[1].split(" ")[1];
 }
+console.log(shortenToDate("Friday May 2, 9am"));
