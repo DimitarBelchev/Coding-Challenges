6a7a63a29e286e243713b3f25c8b3ed65da2b786
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-05-08 23:48:03 +0300

    Update Pick_Peaks.js

--- codewars/Pick_Peaks.js
+++ codewars/Pick_Peaks.js
@@ -34,7 +34,7 @@ function pickPeaks(arr) {
   }
   return result;
 }
-console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]));
+// console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]));
 console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]));
 // console.log(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]));
 // console.log(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]));
