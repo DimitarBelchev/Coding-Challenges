0268e04cf81463480868b3c29a7063d5a77e07fb
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-05-21 21:52:49 +0300

    update

---
+++ other projects/React interview prep/react prep/src/components/Mapandfilter.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "../App.css";
+
+const Mapandfilter = () => {
+  const [users, setUsers] = useState([]);
+  const [numbers, setNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => setUsers(json));
+  }, []);
+
+  const mapData = () => {
+    // let filteredData = users.filter((user) => {
+    // return user.name === "Leanne Graham";
+    // return user.name.includes("L");
+    // return user.id <= 5;
+
+    let square = numbers.map((number) => {
+      return number * number;
+    });
+    console.log(square);
+    // });
+    // setUsers(filteredData);
+    // console.log(filteredData);
+    // let mappedArray = users.map((user) => user.id * 2);
+    // console.log(mappedArray);
+  };
+
+  return (
+    <div className="App">
+      {/* <h1>Users</h1>
+      <div className="card">
+        {users.map((user, index) => (
+          <div key={index} className="card-inner">
+            <p>{user.name}</p>
+            <p>{user.username}</p>
+          </div>
+        ))}
+      </div> */}
+
+      <h1>numbers</h1>
+      <div className="card">
+        {numbers.map((number, index) => (
+          <div key={index} className="card-inner">
+            <p>{number}</p>
+          </div>
+        ))}
+      </div>
+      <button onClick={mapData}>See Mapped Array</button>
+    </div>
+  );
+};
+
+export default Mapandfilter;
