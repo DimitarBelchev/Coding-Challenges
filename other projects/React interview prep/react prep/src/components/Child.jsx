d9d81f086a27ff6ddac903350e737dea4741fea6
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-05-23 14:55:55 +0300

    update

--- other projects/React interview prep/react prep/src/components/Child.jsx
+++ other projects/React interview prep/react prep/src/components/Child.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
-const Child = () => {
-  return <div></div>;
+const Child = ({ name, changeName }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      {/* <button onClick={changeName}>Change Name</button> */}
+      <button onClick={() => changeName("George")}>Change Name</button>
+    </div>
+  );
 };
 
 export default Child;
--- other projects/React interview prep/react prep/src/components/Props.jsx
+++ other projects/React interview prep/react prep/src/components/Props.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import "../App.css";
+import Child from "./Child";
 
 const Props = () => {
   const [name, setName] = useState("Steve");
-  return <div className="App"></div>;
+  const changeName = (value) => {
+    setName(value);
+  };
+  return (
+    <div className="App">
+      <Child name={name} changeName={changeName} />
+      {/* <button onClick={() => setName("George")}>Change Name</button> */}
+    </div>
+  );
 };
 
 export default Props;
