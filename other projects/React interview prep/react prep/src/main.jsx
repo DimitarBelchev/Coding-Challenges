a0f667bab71ffc4752733e5ff3ab2c405822dedd
DimitarBelchev <52967149+DimitarBelchev@users.noreply.github.com>
2023-05-25 23:58:41 +0300

    Update main.jsx

--- other projects/React interview prep/react prep/src/main.jsx
+++ other projects/React interview prep/react prep/src/main.jsx
@@ -17,6 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <Props /> */}
     {/* <CondExpr /> */}
     {/* <Events /> */}
-    <Forms />
+    {/* <Forms /> */}
   </React.StrictMode>
 );
