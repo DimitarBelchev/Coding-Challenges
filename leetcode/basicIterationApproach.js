6ea6ce4bd69b6102c93ecd21814b526732027e01
DimitarBelchev <heavyartillery98@gmail.com>
2024-10-19 21:32:31 +0300

    alt solution

--- leetcode/basicIterationApproach.js
+++ leetcode/basicIterationApproach.js
@@ -26,3 +26,24 @@ checkGridBasic(grid2); //output: false
 
 const grid3 = [[1], [2], [3]];
 checkGridBasic(grid3); //output: false
+
+// function checkGridEvery(grid) {
+//     const m = grid.length;
+//     const n = grid[0].length;
+
+//     return grid.every((row, i) =>
+//         row.every((value, j) =>
+//             (i === m - 1 || value === grid[i + 1][j]) &&
+//             (j === n - 1 || value !== grid[i][j + 1])
+//         )
+//     );
+// }
+
+// const grid1 = [[1,0,2],[1,0,2]];
+// checkGridEvery(grid1);  //output: true
+
+// const grid2 = [[1,1,1],[0,0,0]];
+// checkGridEvery(grid2);  //output: false
+
+// const grid3 = [[1],[2],[3]];
+// checkGridEvery(grid3);  //output: false
