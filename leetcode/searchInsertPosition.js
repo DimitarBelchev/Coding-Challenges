7a7070784718a4d55c679560faeecc77318f952e
DimitarBelchev <heavyartillery98@gmail.com>
2024-11-05 17:35:48 +0200

    update

--- leetcode/searchInsertPosition.js
+++ leetcode/searchInsertPosition.js
@@ -21,3 +21,11 @@ function searchInsertBinarySearch(arr, target) {
 const nums1 = [1, 3, 5, 6];
 const target1 = 5;
 searchInsertBinarySearch(nums1, target1); //output: 2
+
+const nums2 = [1, 3, 5, 6];
+const target2 = 2;
+searchInsertBinarySearch(nums2, target2); //output: 1
+
+const nums3 = [1, 3, 5, 6];
+const target3 = 7;
+searchInsertBinarySearch(nums3, target3); //output: 4
