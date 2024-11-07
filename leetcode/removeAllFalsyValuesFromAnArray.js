aacef09d2b9d8eee31da23cd888ae43c4e18d5fe
DimitarBelchev <heavyartillery98@gmail.com>
2024-11-07 20:12:51 +0200

    Update removeAllFalsyValuesFromAnArray.js

--- leetcode/removeAllFalsyValuesFromAnArray.js
+++ leetcode/removeAllFalsyValuesFromAnArray.js
@@ -1,6 +1,19 @@
-function removeFalsyValuesFilter(arr) {
-  return arr.filter(Boolean);
+// function removeFalsyValuesFilter(arr) {
+//   return arr.filter(Boolean);
+// }
+
+// const array1 = [0, "Hello", false, 42, "", NaN, true, null, "World"];
+// removeFalsyValuesFilter(array1); //output: ["Hello", 42, true, "World"]
+
+function removeFalsyValuesForLoop(arr) {
+  const result = [];
+  for (const value of arr) {
+    if (value) {
+      result.push(value);
+    }
+  }
+  return result;
 }
 
 const array1 = [0, "Hello", false, 42, "", NaN, true, null, "World"];
-removeFalsyValuesFilter(array1); //output: ["Hello", 42, true, "World"]
+removeFalsyValuesForLoop(array1); //output: ["Hello", 42, true, "World"]
