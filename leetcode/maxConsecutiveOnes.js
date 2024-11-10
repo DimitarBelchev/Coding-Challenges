d727537e4a776dbef8249a2c53e03b1d0c5d0472
DimitarBelchev <heavyartillery98@gmail.com>
2024-11-10 18:40:09 +0200

    Update maxConsecutiveOnes.js

--- leetcode/maxConsecutiveOnes.js
+++ leetcode/maxConsecutiveOnes.js
@@ -19,3 +19,28 @@ maxConsecutiveOnesSimple(nums1); //output: 3
 
 const nums2 = [1, 0, 1, 1, 0, 1];
 maxConsecutiveOnesSimple(nums2); //output: 2
+
+// function maxConsecutiveOnesSlidingWindow(nums) {
+//   let left = 0;
+//   let right = 0;
+//   let maxCount = 0;
+
+//   while (right < nums.length) {
+//       if (nums[right] === 1) {
+//           right++;
+//       } else {
+//           maxCount = Math.max(maxCount, right - left);
+//           right++;
+//           left = right;
+//       }
+//   }
+
+//   maxCount = Math.max(maxCount, right - left);
+//   return maxCount;
+// }
+
+// const nums1 = [1,1,0,1,1,1];
+// maxConsecutiveOnesSlidingWindow(nums1);  //output: 3
+
+// const nums2 = [1,0,1,1,0,1];
+// maxConsecutiveOnesSlidingWindow(nums2);  //output: 2
