22713e7a725d2e507b75b8895ad2b7318aec9770
DimitarBelchev <heavyartillery98@gmail.com>
2024-11-13 19:39:36 +0200

    Update maxConsecutiveOnes.js

--- leetcode/maxConsecutiveOnes.js
+++ leetcode/maxConsecutiveOnes.js
@@ -20,27 +20,27 @@ maxConsecutiveOnesSimple(nums1); //output: 3
 const nums2 = [1, 0, 1, 1, 0, 1];
 maxConsecutiveOnesSimple(nums2); //output: 2
 
-function maxConsecutiveOnesSlidingWindow(nums) {
-  let left = 0;
-  let right = 0;
-  let maxCount = 0;
+// function maxConsecutiveOnesSlidingWindow(nums) {
+//   let left = 0;
+//   let right = 0;
+//   let maxCount = 0;
 
-  while (right < nums.length) {
-    if (nums[right] === 1) {
-      right++;
-    } else {
-      maxCount = Math.max(maxCount, right - left);
-      right++;
-      left = right;
-    }
-  }
+//   while (right < nums.length) {
+//     if (nums[right] === 1) {
+//       right++;
+//     } else {
+//       maxCount = Math.max(maxCount, right - left);
+//       right++;
+//       left = right;
+//     }
+//   }
 
-  maxCount = Math.max(maxCount, right - left);
-  return maxCount;
-}
+//   maxCount = Math.max(maxCount, right - left);
+//   return maxCount;
+// }
 
-const nums1 = [1, 1, 0, 1, 1, 1];
-maxConsecutiveOnesSlidingWindow(nums1); //output: 3
+// const nums1 = [1, 1, 0, 1, 1, 1];
+// maxConsecutiveOnesSlidingWindow(nums1); //output: 3
 
-const nums2 = [1, 0, 1, 1, 0, 1];
-maxConsecutiveOnesSlidingWindow(nums2); //output: 2
+// const nums2 = [1, 0, 1, 1, 0, 1];
+// maxConsecutiveOnesSlidingWindow(nums2); //output: 2
