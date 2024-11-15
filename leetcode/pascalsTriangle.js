9fc5886acb64f611b3ecb5e32c2af08df24ef152
DimitarBelchev <heavyartillery98@gmail.com>
2024-11-15 09:06:20 +0200

    Update pascalsTriangle.js

---
+++ leetcode/pascalsTriangle.js
@@ -0,0 +1,24 @@
+function generatePascalTriangleRecursion(numRows) {
+  if (numRows === 0) return [];
+  if (numRows === 1) return [[1]];
+
+  const triangle = [[1]];
+
+  for (let i = 1; i < numRows; i++) {
+    const newRow = new Array(i + 1).fill(1);
+
+    for (let j = 1; j < i; j++) {
+      newRow[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+    }
+
+    triangle.push(newRow);
+  }
+
+  return triangle;
+}
+
+const input1 = 5;
+generatePascalTriangleRecursion(input1); //output:  [ [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1] ]
+
+const input2 = 1;
+generatePascalTriangleRecursion(input2); //output: [[1]]
