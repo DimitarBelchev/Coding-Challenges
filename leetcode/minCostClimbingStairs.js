e3c1c4c524727e74c6fcfa7f7b2d3bc97794b613
DimitarBelchev <heavyartillery98@gmail.com>
2024-12-19 11:12:10 +0200

    Update minCostClimbingStairs.js

--- leetcode/minCostClimbingStairs.js
+++ leetcode/minCostClimbingStairs.js
@@ -1,24 +1,24 @@
-function minCostHashMap(cost) {
-  const memo = new Map();
+// function minCostHashMap(cost) {
+//   const memo = new Map();
 
-  function dp(i) {
-    if (i < 0) return 0;
-    if (i === 0 || i === 1) return cost[i];
-    if (memo.has(i)) return memo.get(i);
-    const result = cost[i] + Math.min(dp(i - 1), dp(i - 2));
-    memo.set(i, result);
-    return result;
-  }
+//   function dp(i) {
+//     if (i < 0) return 0;
+//     if (i === 0 || i === 1) return cost[i];
+//     if (memo.has(i)) return memo.get(i);
+//     const result = cost[i] + Math.min(dp(i - 1), dp(i - 2));
+//     memo.set(i, result);
+//     return result;
+//   }
 
-  const n = cost.length;
-  return Math.min(dp(n - 1), dp(n - 2));
-}
+//   const n = cost.length;
+//   return Math.min(dp(n - 1), dp(n - 2));
+// }
 
-const cost1 = [10, 15, 20];
-minCostHashMap(cost1); //output: 15
+// const cost1 = [10, 15, 20];
+// minCostHashMap(cost1); //output: 15
 
-const cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
-minCostHashMap(cost2); //output: 6
+// const cost2 = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+// minCostHashMap(cost2); //output: 6
 
 function minCostFullTable(cost) {
   const dp = Array(cost.length).fill(0);
