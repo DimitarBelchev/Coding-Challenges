0d5997793835758f7d22e37d19aac5a5207116ad
DimitarBelchev <heavyartillery98@gmail.com>
2025-01-11 23:29:27 +0200

    Delete Testing123.js

--- codewars/Testing123.js
+++
@@ -1,9 +0,0 @@
-var number = function (array) {
-  //your awesome code here
-  finalArr = [];
-  for (i = 0; i < array.length; i++) {
-    array[i] = `${i + 1}: ${array[i]}`;
-  }
-  return array;
-};
-console.log(number(["a", "b", "c"]));
